5dc5fdfb6f5da65ecc08a6891dbfc375b58e966d
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-02-01 22:34:19 +0900

    debugを追加

--- command.py
+++ command.py
@@ -9,15 +9,19 @@ import json
 from bubble_msg import taskBubbleMsg
 GAS_URL = "https://script.google.com/macros/s/AKfycby8acn6-HFL9snjXpYp1bK8S8Ju7w6WR4la6znsMjJNpvsDLSnZl0D-UtyfG2P_o1JL/exec"
 class Commands(object):
-    def __init__(self,channel_access_token, reply_token):
+    def __init__(self,channel_access_token, reply_token, debug=False):
         """_summary_
 
         Args:
             channel_access_token (str): linebotのチャンネルアクセストークン
             reply_token (str): webhookで受け取ったリプライトークン
+            debug (bool, optional): デバッグモードかどうか
         """
-
-        self.line_bot_api = LineBotApi(channel_access_token)
+        self.debug = debug
+        if debug:
+            self.line_bot_api = None
+        else:
+            self.line_bot_api = LineBotApi(channel_access_token)
         self.reply_token = reply_token
         
     def process(self, cmd):
@@ -136,8 +140,11 @@ class Commands(object):
             print(e)
 
     def _reply_text_message(self, text):
-        self.line_bot_api.reply_message(
-            self.reply_token, TextSendMessage(text=text)
+        if self.debug:
+            print(text)
+        else:
+            self.line_bot_api.reply_message(
+                self.reply_token, TextSendMessage(text=text)
             )
     
     def _calculate_date_difference(iso_datetime: str, tz_offset_hours: int = 0):
@@ -165,3 +172,6 @@ class Commands(object):
         day_difference = (local_date - today).days
 
         return day_difference
+
+if __name__ == "__main__":
+    Commands("test", "test",debug=True).process("!reminder")
