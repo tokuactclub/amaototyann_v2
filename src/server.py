ae3580b32f3228b4f5c1748deb75cee5537989bd
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-13 02:03:35 +0900

    move transribewebhook to system

--- src/server.py
+++ src/server.py
@@ -9,7 +9,7 @@ from linebot.models import TextSendMessage
 import json
 
 from src.command import Commands, CommandsScripts
-from src.system import BotInfo
+from src.system import BotInfo, transcribeWebhook
 from src import messages
 # ローカル開発の場合.envファイルから環境変数を読み込む
 # IS_RENDER_SERVER が存在しない場合はローカル開発と判断
@@ -51,46 +51,7 @@ if  not server_boot_script_running or server_boot_script_running == "False":
     thread = threading.Thread(target=boot_server)
     thread.start()
 
-# webhookを転送する関数
-def transcribeWebhook(request, url, body=None):
-    """webhookを転送する関数
 
-    Args:
-        request (Request): リクエスト
-        url (str): 転送先のURL
-        body (dict, optional): リクエストボディを変えたい場合指定
-
-    Returns:
-        Response: 転送先からのレスポンス
-    """
-    method = request.method
-    logger.info(f"headersType:{type(request.headers)}")
-    headers = {key: value for key, value in dict(request.headers).items() if key != 'Host'} 
-    if(not body):#bodyを指定されなければeventのbodyを利用（本来の挙動）
-        body = request.json
-    
-    logger.info(f"Method: {method}Type:{type(method)}")
-    logger.info(f"URL : {url}Type:{type(url)}")
-    logger.info(f"Headers: {headers}Type:{type(headers)}")
-    logger.info(f"Body: {body}Type:{type(body)}")
-
-    try:
-        # Reconstruct headers and forward the request
-        headers["Content-Type"] = "application/json;charset=utf-8"
-        response = requests.request(
-            method=method,
-            url=url,
-            headers=json.loads(json.dumps(headers)),
-            json=json.loads(json.dumps(body)),
-        )
-
-        logger.info('Forwarded Data:', response)
-        logger.info('HTTP Status Code:', response.status_code)
-
-        return 'Data forwarded successfully', 200
-    except Exception as e:
-        logger.error('Error:', e)
-        return 'Failed to forward data', 500
 
     
 # Flaskのインスタンスを作成
--- src/system.py
+++ src/system.py
@@ -42,3 +42,44 @@ class BotInfo():
         else:
             logger.error(f"Failed to get all data: {response.status_code} - {response.text}")
             return None
+        
+# webhookを転送する関数
+def transcribeWebhook(request, url, body=None):
+    """webhookを転送する関数
+
+    Args:
+        request (Request): リクエスト
+        url (str): 転送先のURL
+        body (dict, optional): リクエストボディを変えたい場合指定
+
+    Returns:
+        Response: 転送先からのレスポンス
+    """
+    method = request.method
+    logger.info(f"headersType:{type(request.headers)}")
+    headers = {key: value for key, value in dict(request.headers).items() if key != 'Host'} 
+    if(not body):#bodyを指定されなければeventのbodyを利用（本来の挙動）
+        body = request.json
+    
+    logger.info(f"Method: {method}Type:{type(method)}")
+    logger.info(f"URL : {url}Type:{type(url)}")
+    logger.info(f"Headers: {headers}Type:{type(headers)}")
+    logger.info(f"Body: {body}Type:{type(body)}")
+
+    try:
+        # Reconstruct headers and forward the request
+        headers["Content-Type"] = "application/json;charset=utf-8"
+        response = requests.request(
+            method=method,
+            url=url,
+            headers=json.loads(json.dumps(headers)),
+            json=json.loads(json.dumps(body)),
+        )
+
+        logger.info('Forwarded Data:', response)
+        logger.info('HTTP Status Code:', response.status_code)
+
+        return 'Data forwarded successfully', 200
+    except Exception as e:
+        logger.error('Error:', e)
+        return 'Failed to forward data', 500
