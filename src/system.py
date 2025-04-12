d124c6a001c36a6f2f77b775ae0256da79c10866
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-13 00:12:44 +0900

    add log

--- src/system.py
+++ src/system.py
@@ -1,6 +1,12 @@
 import requests
 import os
 from pprint import pprint
+import json
+# loggerの設定
+from logging import getLogger, config
+with open("src/log_config.json", "r") as f:
+    config.dictConfig(json.load(f))
+logger = getLogger(__name__)
 
 class BotInfo():
     def __init__(self):
@@ -12,6 +18,7 @@ class BotInfo():
         if response.status_code == 200:
             return response.json()
         else:
+            logger.error(f"Failed to get data for ID {id}: {response.status_code} - {response.text}")
             return None
     
     def update(self, id, column, value):
@@ -20,6 +27,7 @@ class BotInfo():
         if response.status_code == 200:
             return response.json()
         else:
+            logger.error(f"Failed to update data for ID {id}: {response.status_code} - {response.text}")
             return None
         
     def get_all(self):
@@ -28,4 +36,5 @@ class BotInfo():
         if response.status_code == 200:
             return response.json()
         else:
+            logger.error(f"Failed to get all data: {response.status_code} - {response.text}")
             return None
