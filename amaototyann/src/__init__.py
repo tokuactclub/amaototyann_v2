bf4a55f2e4ca3d93e9d5b93ad6d392cf551e3866
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-14 19:11:51 +0900

    logファイルの場所を変更

--- amaototyann/src/__init__.py
+++ amaototyann/src/__init__.py
@@ -3,6 +3,10 @@ import json
 import os
 from logging import getLogger, config
 
+# logs/app.logのフォルダ・ファイルが存在しない場合は作成
+if not os.path.exists("amaototyann/logs"):
+    os.makedirs("amaototyann/logs")
+
 with open("amaototyann/src/log_config.json", "r") as f:
     config.dictConfig(json.load(f))
 logger = getLogger("logger")
