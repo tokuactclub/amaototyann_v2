abf11f70aade4968827f36985ebfc464405f3c2d
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-15 15:27:15 +0900

    マルチプロセスの代わりに非同期処理を利用

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
-gunicorn amaototyann.src.server:app --timeout 300 -w 4
+# ワーカーを増やす（＝プロセスを増やす）と、データの共有をモジュールで行っているがために、データが共有されなくなる。
+# そのため、ワーカーは1つにしている。
+# もし、ワーカーを増やす場合は、データの共有を行う方法を考える必要がある。
+# 現在は、gunicornの--worker-classをgeventにして、非同期で処理速度を向上させる
+gunicorn amaototyann.src.server:app --worker-class gevent --timeout 300 -w 1
 
