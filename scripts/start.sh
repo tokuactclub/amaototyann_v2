ff60a9fb6c7edeaf991aa0fea193cb78f03a7d6d
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-15 19:38:26 +0900

    version check

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,6 @@
 # もし、ワーカーを増やす場合は、データの共有を行う方法を考える必要がある。
 # もしくは、gunicornの--worker-classをgeventにして、非同期で処理速度を向上させる
 # どうやらこれを利用するとエラーが出るので、一旦単一ワーカーで動かす。まあ大丈夫っしょ。
-# gunicorn amaototyann.src.server:app --worker-class gevent --timeout 300 -w 1
-gunicorn amaototyann.src.server:app  --timeout 300 -w 1
+gunicorn amaototyann.src.server:app --worker-class gevent --timeout 300 -w 1
+# gunicorn amaototyann.src.server:app  --timeout 300 -w 1
 
