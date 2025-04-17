64a301a425887e2193ed58e734965e02a8aa698c
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2025-04-17 23:36:21 +0900

    update start  setting

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,39 @@
-# ワーカーを増やす（＝プロセスを増やす）と、データの共有をモジュールで行っているがために、データが共有されなくなる。
-# そのため、ワーカーは1つにしている。
-# もし、ワーカーを増やす場合は、データの共有を行う方法を考える必要がある。
-# もしくは、gunicornの--worker-classをgeventにして、非同期で処理速度を向上させる
-# どうやらこれを利用するとエラーが出るので、一旦単一ワーカーで動かす。まあ大丈夫っしょ。
-# gunicorn amaototyann.src.server:app --worker-class gevent --timeout 300 -w 1
-gunicorn amaototyann.src.server:app  --timeout 300 -w 1
 
+wait_for_log_string() {
+    local TARGET="$1"
+    local LOG_FILE="./amaototyann/logs/app.log"
+
+    if [ ! -f "$LOG_FILE" ]; then
+        # 作成
+        mkdir -p "$(dirname "$LOG_FILE")"
+    fi
+
+    # 現在のログの行数を記録（ここ以前は無視）
+    START_LINE=$(wc -l < "$LOG_FILE")
+
+    # tailで現在の行数以降を監視
+    tail -n +$((START_LINE + 1)) -F "$LOG_FILE" | while IFS= read -r line; do
+        echo "$line" | grep -F "$TARGET">/dev/null
+        if echo "$line" | grep -qF "$TARGET" || echo "$line" | grep -qF "$KEYWORD"; then
+            echo "Flask is up!"
+            break
+        fi
+    done
+}
+
+echo "=========================="
+echo "Starting database..."
+echo "=========================="
+# DB起動して出力を監視
+python3 -m amaototyann.src.database &
+DB_PID=$!
+# wait for server to start
+
+wait_for_log_string "* Running on http://127.0.0.1:5000"
+
+echo "==========================="
+echo "Database started."
+echo "Starting server..."
+echo "==========================="
+# start server
+gunicorn amaototyann.src.server:app --timeout 300 -w 1
